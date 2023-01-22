960710569411f25bdde2d9c406ebd5aa888404fd
Abdullah Al Mahmud <104672914+mahmud5658@users.noreply.github.com>
2023-01-23 02:07:45 +0600

    do while loop

---
+++ do_while_loop.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int n,i=0;
+
+    printf("Enter a number do you want to print: ");
+
+    scanf("%d",&n);
+
+    do {
+
+        printf("Daffodil International University.\n");
+
+        i++;
+
+    }
+
+    while(i<n);
+    
+    return 0;
+}
