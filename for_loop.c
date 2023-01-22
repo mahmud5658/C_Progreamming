aa18c15ab047e85f9d02cc5c08e01eae45cdef1a
Abdullah Al Mahmud <104672914+mahmud5658@users.noreply.github.com>
2023-01-23 02:08:16 +0600

    for loop

---
+++ for_loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    printf("Enter a number do want to print: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        printf("Daffodil International University.\n");
+    }
+    return 0;
+}
