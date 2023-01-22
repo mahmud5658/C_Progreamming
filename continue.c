2454a1dbfb75ec79b8683eef49f42754f988d2bd
Abdullah Al Mahmud <104672914+mahmud5658@users.noreply.github.com>
2023-01-23 02:16:10 +0600

    continue statement

---
+++ continue.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+    int n;
+
+    printf("Enter a number: ");
+
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+
+        if (i==5){
+
+            continue;
+
+        }
+
+        printf("%d\t",i);
+
+    }
+    return 0;
+}
